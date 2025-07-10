2171c1334967f14a4c096f2f9d95bee14618a90e
Henrique Barros <henriquebarrosx@gmail.com>
2025-07-10 10:31:16 -0300

    cria projeto spring

---
+++ src/main/java/com/magalu/comunicados/ComunicadosApplication.java
@@ -0,0 +1,13 @@
+package com.magalu.comunicados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magalu/comunicados/ComunicadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalu.comunicados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
